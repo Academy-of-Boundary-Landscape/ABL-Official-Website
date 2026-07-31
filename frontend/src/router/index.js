437b166f91faac2e0335288db62c02d76505f364
Renko6626 <2300011496@stu.pku.edu.cn>
2026-07-31 17:45:10 +0800

    perf: :zap: 路由改为懒加载，详情页依赖移出首屏包

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,5 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import RecruitmentView from '../views/RecruitmentView.vue'
-import zyzView from '../views/zyzView.vue'
-import ProductList from '../views/ProductList.vue'
-import ProductDetail from '../views/ProductDetail.vue'
-import EventList from '../views/EventList.vue'
-import EventDetail from '../views/EventDetail.vue'
-import csd20View from '../views/projects/csd20.vue'
-import csd20musicView from '../views/projects/csd20music.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,48 +7,48 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => import('../views/HomeView.vue'),
     },
     {
       path: '/recruitment',
       name: 'recruitment',
-      component: RecruitmentView,
+      component: () => import('../views/RecruitmentView.vue'),
     },
     {
       path: '/project/zhu-yuanzhang', // 定义一个清晰的URL
       name: 'zhu-yuanzhang',
-      component: zyzView,
+      component: () => import('../views/zyzView.vue'),
     },
     {
       path: '/products',
       name: 'products',
-      component: ProductList,
+      component: () => import('../views/ProductList.vue'),
     },
     {
       path: '/products/:slug', // 路径中包含一个名为 "slug" 的动态参数
       name: 'ProductDetail',
-      component: ProductDetail,
+      component: () => import('../views/ProductDetail.vue'),
       // Vue Router 会自动将URL中的 slug 部分作为参数传递给组件
     },
     {
       path: '/events',
       name: 'EventList',
-      component: EventList,
+      component: () => import('../views/EventList.vue'),
     },
     {
       path: '/events/:slug',
       name: 'EventDetail',
-      component: EventDetail,
+      component: () => import('../views/EventDetail.vue'),
     },
     {
       path: '/project/csd20',
       name: 'csd20',
-      component: csd20View,
+      component: () => import('../views/projects/csd20.vue'),
     },
     {
       path: '/project/csd20/music',
       name: 'csd20music',
-      component: csd20musicView,
+      component: () => import('../views/projects/csd20music.vue'),
     },
   ],
 })
